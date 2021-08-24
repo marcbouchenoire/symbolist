61b86d893d536dca373b8776a2e6896075e1ad19
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2021-08-24 21:21:48 +0200

    Ignore Next's ESLint integration

--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -2,6 +2,9 @@ module.exports = {
   basePath: "/ios-symbols",
   cleanUrls: true,
   trailingSlash: false,
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   async redirects() {
     return [
       {
