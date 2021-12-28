d5bac57a9b64d854eabe4fecb8fa3123bad363f0
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2021-12-28 11:12:01 +0100

    Improve meta tags

--- packages/site/src/pages/_app.tsx
+++ packages/site/src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from "next-themes"
 import { AppProps } from "next/app"
 import Head from "next/head"
-import { Favicon } from "../components/Favicon"
 import "../styles/main.css"
 
 /**
@@ -16,19 +15,45 @@ function App({ Component, pageProps }: AppProps) {
     <ThemeProvider attribute="class" disableTransitionOnChange>
       <Head>
         <title>Symbolist</title>
+        <meta content="Symbolist" name="title" />
+        <meta content="Marc Bouchenoire" name="author" />
         <meta content="initial-scale=1, viewport-fit=cover" name="viewport" />
         <meta
           content="A collection of every symbol from SF Symbols."
           name="description"
         />
-        <meta content="Symbolist" property="og:title" />
+        <meta content="website" property="og:type" />
+        <meta
+          content="https://symbolist.marcbouchenoire.com/"
+          property="og:url"
+        />
+        <meta content="Marc Bouchenoire" property="og:title" />
         <meta
           content="A collection of every symbol from SF Symbols."
           property="og:description"
         />
-        <meta content="@marcbouchenoire" name="twitter:creator" />
+        <meta
+          content="https://symbolist.marcbouchenoire.com/meta.png"
+          property="og:image"
+        />
+        <meta
+          content="https://www.marcbouchenoire.com/"
+          property="twitter:url"
+        />
+        <meta content="@marcbouchenoire" property="twitter:creator" />
+        <meta content="Symbolist" property="twitter:title" />
+        <meta
+          content="A collection of every symbol from SF Symbols."
+          property="twitter:description"
+        />
+        <meta
+          content="https://symbolist.marcbouchenoire.com/meta.png"
+          property="twitter:image"
+        />
+        <link href="/favicon.png" rel="icon" sizes="any" type="image/png" />
+        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        <meta content="#e37" name="theme-color" />
       </Head>
-      <Favicon>🔣</Favicon>
       <Component {...pageProps} />
     </ThemeProvider>
   )
