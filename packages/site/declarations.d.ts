054c8d058b734bca3e15254896cebff03dfefc60
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-01-09 21:51:55 +0100

    Remove custom Tailwind declarations

--- packages/site/declarations.d.ts
+++ packages/site/declarations.d.ts
@@ -1,55 +1,5 @@
-/* eslint-disable jsdoc/require-jsdoc */
-
 declare module "*/data.json" {
   const value: any
 
   export default value
-}
-
-declare module "tailwindcss/colors" {
-  interface ColorGroup {
-    readonly 100: string
-    readonly 200: string
-    readonly 300: string
-    readonly 400: string
-    readonly 50: string
-    readonly 500: string
-    readonly 600: string
-    readonly 700: string
-    readonly 800: string
-    readonly 900: string
-  }
-
-  interface Colors {
-    amber: ColorGroup
-    black: string
-    blue: ColorGroup
-    cyan: ColorGroup
-    emerald: ColorGroup
-    fuchsia: ColorGroup
-    gray: ColorGroup
-    green: ColorGroup
-    indigo: ColorGroup
-    inherit: string
-    lime: ColorGroup
-    neutral: ColorGroup
-    orange: ColorGroup
-    pink: ColorGroup
-    purple: ColorGroup
-    red: ColorGroup
-    rose: ColorGroup
-    sky: ColorGroup
-    slate: ColorGroup
-    stone: ColorGroup
-    teal: ColorGroup
-    transparent: string
-    violet: ColorGroup
-    white: string
-    yellow: ColorGroup
-    zinc: ColorGroup
-  }
-
-  declare const colors: Colors
-
-  export = colors
 }
